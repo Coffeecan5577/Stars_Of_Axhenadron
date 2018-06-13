c6bf7ee675754a572c2907af2a895016b424afe3
Cale Curry <c.curry2210@gmail.com>
2018-06-13 16:39:10 -0500

    Created a splash screen, and added background music with a working level manager.

---
+++ Assets/Scripts/ManageLevels.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageLevels : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Invoke("LoadLevel", 2.0f);
+	}
+
+    private void LoadLevel()
+    {
+        int currentLevel = SceneManager.GetActiveScene().buildIndex;
+        int nextLevel = currentLevel + 1;
+        int levelCount = SceneManager.sceneCount;
+
+        if (currentLevel < levelCount)
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+        else
+        {
+            // Reload the splash screen.
+            SceneManager.LoadScene(0);
+        }
+    }
+
+}
