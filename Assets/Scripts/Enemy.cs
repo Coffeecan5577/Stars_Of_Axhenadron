5f700b121ae6494995d6e84d3e5cfeab4a625c57
Cale Curry <c.curry2210@gmail.com>
2018-06-18 18:47:15 -0500

    Enemies are now destroyed by player bullets.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemyDeathExplosion;
+    private float _explosionResetDelay = 0.2f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnParticleCollision(GameObject other)
+    {
+        print("Particles collided with enemy: " + gameObject.name);
+        DestroyEnemy();
+       // Invoke("ResetExplosion", _explosionResetDelay);
+    }
+
+    private void DestroyEnemy()
+    {
+        //_enemyDeathExplosion.SetActive(true);
+        Destroy(gameObject);
+       
+    }
+
+    //private void ResetExplosion()
+    //{
+    //    _enemyDeathExplosion.SetActive(false);
+    //}
+}
