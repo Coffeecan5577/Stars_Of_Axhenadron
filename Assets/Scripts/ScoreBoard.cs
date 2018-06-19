1510473b6521f4b52d85d71df0eadc2ab8b06640
Cale Curry <c.curry2210@gmail.com>
2018-06-18 20:10:42 -0500

    Score can now increment through a public method. Will link up in next commit.

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] int scorePerHit = 12;
+
+    private int currentScore = 0;
+    private Text scoreText;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    scoreText = GetComponent<Text>();
+	    scoreText.text = currentScore.ToString();
+	}
+
+    public void ScoreHit()
+    {
+        currentScore += scorePerHit;
+        scoreText.text = currentScore.ToString();
+    }
+}
