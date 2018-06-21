ec0046bf9c87b11516ea3e2fd71bb69ae1db8c84
Cale Curry <c.curry2210@gmail.com>
2018-06-21 13:15:29 -0500

    Enemies now have a working health system.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
     [SerializeField] private GameObject deathFX;
     [SerializeField] private Transform parent;
     [SerializeField] private int scorePerHit;
+    [SerializeField] private int hits = 10;
 
     private ScoreBoard scoreBoard;
 
@@ -20,14 +21,24 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
-        scoreBoard.ScoreHit(scorePerHit);
-        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
-        fx.transform.parent = parent;
-        DestroyEnemy();
+        ProcessHit();
+        if (hits <= 0)
+        {
+            DestroyEnemy();
+        }
     }
+
+    private void ProcessHit()
+    {
+        scoreBoard.ScoreHit(scorePerHit);
+        hits -= 1;
+    }
+
 
     private void DestroyEnemy()
     {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 
